c65c0197342635c1b0049266895ff59be14bfb4c
Brian Alonzo <connect@brianalonzo.com>
2020-05-01 14:13:50 -0400

    Title Change

--- index.php
+++ index.php
@@ -107,7 +107,7 @@ Brian Alonzo - A Web Design, Development &amp; Digital Marketing Consultant.    
           <div class="container">
             <div class="row">
               <div class="col-lg-12 mx-auto">
-                  <span class="main-title">I Am A Web Marketing Consultant.</span>
+                  <span class="main-title">I Am A Web Developer.</span>
                   <div class="video">
                       <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/295618283?portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
                   </div>
